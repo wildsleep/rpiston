a6152f63cfd841fe533b046a73ff4c8e7b6f20d4
Wild Sleep <wildsleep@mailinator.com>
2015-04-08 04:39:29 +0000

    Added option to save analogWrite values to EEPROM on MCP

--- mcp4725.js
+++ mcp4725.js
@@ -7,14 +7,21 @@ function handleError(error) {
 		console.error(error);
 }
 
-function analogWrite(value) {
+function analogWrite(value, writeToEeprom) {
+	// LERP from [0.0, 1.0] to [0x000, 0xFFF]
+	var twelveBitValue = (value * 0xFFF) | 0;
+
+	// Construct packet
 	// See data sheet, page 25 (fig. 6-2)
 	// https://cdn.sparkfun.com/datasheets/BreakoutBoards/MCP4725_2009.pdf
-	var twelveBitValue = (value * 0xFFF) | 0;
-	var highByte = twelveBitValue >> 4;
-	var lowByte = (twelveBitValue & 0xF) << 4;
-	wire.writeBytes(0x40, [highByte, lowByte], handleError);
-};
+	var secondByte =
+		(1 << 6) | // .................................. C1
+		((writeToEeprom ? 1 : 0) << 5); // ............. C0
+	var thirdByte = twelveBitValue >> 4; // ............ D11-D4
+	var fourthByte = (twelveBitValue & 0xF) << 4; // ... D3-D0
+
+	wire.writeBytes(secondByte, [thirdByte, fourthByte], handleError);
+}
 
 module.exports = { analogWrite: analogWrite };
 
